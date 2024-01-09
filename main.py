250d396b0680e1287e52c8ff212e849dd8353dfa
vivy-c <cahyanivivy@gmail.com>
2024-01-09 23:58:43 +0700

    [IMP] Error diff type data

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ sapa = 'Halo semua .... saya '
 namasaya = 'Vivy Cahyani' #type string
 usia = 22 # type int
 tinggi_badan = 1.7 # type data float
+usiaBaru = "18" #string
+
 #print
 print('Halo nama aku vivy')
 print(sapa + namasaya)
-print( usia)
+print('Usia saya ' + usia)# error harus satu type data
 print(tinggi_badan)
