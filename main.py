d1033468676be3d815b888d1e0e43cf35ed362a9
vivy-c <cahyanivivy@gmail.com>
2024-01-09 23:55:59 +0700

    [IMP] Several data type example

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
-namasaya = 'Vivy Cahyani'
+sapa = 'Halo semua .... saya '
+namasaya = 'Vivy Cahyani' #type string
+usia = 22 # type int
+tinggi_badan = 1.7 # type data float
 #print
 print('Halo nama aku vivy')
-print(namasaya)
+print(sapa + namasaya)
+print( usia)
+print(tinggi_badan)
