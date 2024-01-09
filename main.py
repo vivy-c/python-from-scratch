c5c819e533ab5c04c0dca5e3b83dd4046b4a4aa3
vivy-c <cahyanivivy@gmail.com>
2024-01-10 00:22:40 +0700

    [IMP] Try variable and input function

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
+input_nama = input('Halo, siapa nama kamu? ')
 
-sapa = 'Halo semua .... nama saya '
-namasaya = 'Vivy Cahyani' #type string
-usia = 22 # type int
-tinggi_badan = 1.7 # type data float
-punya_hewan_peliharaan = False
-punya_hewan_peliharaan = True # type boolean (True / False)
-
-#print
-print(sapa + namasaya + ' usia saya ' + str(usia))
-print('Tinggi badan ' + str(tinggi_badan) + ' cm')
-print(punya_hewan_peliharaan)
+print(input_nama)
