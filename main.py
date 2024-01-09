0f69da786e1ad59d2b0c8a36e535eb7461530453
vivy-c <cahyanivivy@gmail.com>
2024-01-10 00:03:57 +0700

    [IMP] Try data type conversion

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ usia = 22 # type int
 tinggi_badan = 1.7 # type data float
 usiaBaru = "18" #string
 
+#konversi
+# int("10") ---> 10
+# str(10) ---> "10"
+# float(5) ---> 5.0
+
 #print
 print('Halo nama aku vivy')
 print(sapa + namasaya)
-print('Usia saya ' + usia)# error harus satu type data
+print('Usia saya ' + usiaBaru)
+print('Usia saya ' + str(usia))
 print(tinggi_badan)
