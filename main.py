79e83c29a2f00987deb61d5f2060cb36a72f321a
vivy-c <cahyanivivy@gmail.com>
2024-01-11 14:12:59 +0700

    [IMP] Simple case study

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# gabungan input dengan operasi aritmatika
-a = input('masukan angka pertama = ') # ouput inputan string misal 3 menjadi '3'
-b = input('masukan angka kedua = ') # inputan 8 menjadi '8'
-# c = a + b # hasilnya menjadi 38
+saldo_awal = input('Berapa saldo awalmu : ')
+deposit = input('Berapa mau depositnya? ')
 
-# harus di konvert dulu menjadi integer
-c = int(a) + int(b) # disini hasilnya menjadi 12
-d = int(a) - int(b)
-e = int(a) * int(b)
-f = int(a) / int(b)
-# f = int(a) // int(b) # pakai dua garis kalau ingin dibulatkan kebawah hasilnya
+jumlah = int(saldo_awal) + int(deposit)
 
-hello = 'hello nama saya ' + 'hello test'
-
-print('hasilnya penjumlahannya adalah ' + str(c) + ' pengurangannya ' + str(d) + ' pembagiannya ' + str(e) + ' perkaliannya ' + str(f)) #disini jadi string
-print(hello)
+print('Jumlahnya adalah ' + str(jumlah))
