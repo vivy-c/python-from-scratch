6abd933989f10d6b36a8204cd818cd8bb0ccea3c
vivy-c <cahyanivivy@gmail.com>
2024-01-11 14:02:00 +0700

    [IMP] Input & arithmatic calculation

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# operasi aritmatika python ( + - * / )
+# gabungan input dengan operasi aritmatika
+a = input('masukan angka pertama = ') # ouput inputan string misal 3 menjadi '3'
+b = input('masukan angka kedua = ') # inputan 8 menjadi '8'
+# c = a + b # hasilnya menjadi 38
 
-a = 20
-b = 30
+# harus di konvert dulu menjadi integer
+c = int(a) + int(b) # disini hasilnya menjadi 12
+d = int(a) - int(b)
+e = int(a) * int(b)
+f = int(a) / int(b)
+# f = int(a) // int(b) # pakai dua garis kalau ingin dibulatkan kebawah hasilnya
 
-c = a + b
-d = a - b
-e = a * b
-f = a / b #type data mengikuti hasil perhitungan
+hello = 'hello nama saya ' + 'hello test'
 
-print(c)
-print(d)
-print(e)
-print(f)
+print('hasilnya penjumlahannya adalah ' + str(c) + ' pengurangannya ' + str(d) + ' pembagiannya ' + str(e) + ' perkaliannya ' + str(f)) #disini jadi string
+print(hello)
