43cfbb0a998a97ca58bba13b189c4b2eddf263b5
vivy-c <cahyanivivy@gmail.com>
2024-01-11 14:28:12 +0700

    [IMP] String in depth

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-saldo_awal = input('Berapa saldo awalmu : ')
-deposit = input('Berapa mau depositnya? ')
+# FIND & LEN
 
-jumlah = int(saldo_awal) + int(deposit)
+nama_saya = "vivy cahyani"
 
-print('Jumlahnya adalah ' + str(jumlah))
+# find pada str tiap characternya sesuai index
+print(nama_saya.find("v"))
+print(nama_saya.find("a"))
+print(nama_saya.find("y"))
+print(nama_saya.find("V"))
+
+# len pada str untuk menghitung jumlah characternya
+print(len(nama_saya))
+
+# bisa juga cek dengan boolean
+print('z' in nama_saya)
+print('y' in nama_saya)
+print('C' in nama_saya)
+print('c' in nama_saya)
+
+# operasi huruf
+print(nama_saya.upper())
+print(nama_saya.capitalize())
+print(nama_saya.count('v')) #dan masih banyak fungsi lainnya ada juga suggestion ketika mengetiknya
